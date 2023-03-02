ede1fa222a69f5a948c344e8f55987a3e026ba7a
thekiborjepkorir <edithkibor2005@gmail.com>
2023-03-02 12:01:50 +0300

    initial commit

---
+++ crudjepkorir/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class student(models.Model):
+    name=models.CharField(max_length=30,blank=False,null=False)
+    email=models.EmailField()
+    age=models.IntegerField()
+    gender=models.CharField(max_length=30, blank=False, null=False)
+
+
+def __str__(self):
+    return self.name
